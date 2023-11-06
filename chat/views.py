efd9b157678a872af78151366c905841feae4025
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-11-06 11:10:23 +0700

    setting pub sub

---
+++ chat/views.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.shortcuts import render
+from django.http import HttpResponse
+import paho.mqtt.client as mqtt
+import json
+from django.http import JsonResponse
+from chat.mqtt import client as mqtt_client
+from django.views.decorators.csrf import csrf_exempt
+
+def index(request):
+    return HttpResponse("Hello, chat!")
+
+@csrf_exempt
+def publish_message(request):
+    request_data = json.loads(request.body)
+    rc, mid = mqtt_client.publish(request_data['topic'], request_data['msg'])
+    return JsonResponse({'code': rc})
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chat.apps.ChatConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MQTT_SERVER = 'broker.emqx.io'
+MQTT_PORT = 1883
+MQTT_KEEPALIVE = 60
+MQTT_USER = 'emqx'
+MQTT_PASSWORD = 'public'
